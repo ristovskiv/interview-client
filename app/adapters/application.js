f9ad6db4bb4bdbff674082160c223bd8db47e891
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2016-12-29 14:30:35 +0100

    chore: override application seralizer and adapter for compatibility with rails api json-api specification

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,27 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin,{
   host: `${config.host}`,
-  authorizer: 'authorizer:custom'
+  authorizer: 'authorizer:custom',
+  // allows the multiword paths in urls to be underscored
+  pathForType: function(type) {
+    let underscored = Ember.String.underscore(type);
+    return Ember.String.pluralize(underscored);
+  },
+
+  // allows queries to be sent along with a findRecord
+  // hopefully Ember / EmberData will soon have this built in
+  // ember-data issue tracked here:
+  // https://github.com/emberjs/data/issues/3596
+  urlForFindRecord(id, modelName, snapshot) {
+    let url = this._super(...arguments);
+    let query = Ember.get(snapshot, 'adapterOptions.query');
+    if(query) {
+      url += '?' + Ember.$.param(query);
+    }
+    return url;
+  }
 });
