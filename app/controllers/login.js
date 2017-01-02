2e9871d1fea19372ef4cfbfa1290e1df1bd73504
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2017-01-02 18:25:29 +0100

    feat: introduce ember-cli-flash add-on for flash messages

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,9 +6,13 @@ export default Ember.Controller.extend({
   actions: {
     authenticate: function() {
       let credentials = this.getProperties('identification', 'password'), authenticator = 'authenticator:jwt';
-      this.get('session').authenticate(authenticator, credentials).catch((reason)=>{
-        this.set('errorMessage', reason.error || reason);
-      });
+      this.get('session').authenticate(authenticator, credentials).then(
+          ()=>{
+            this.get('flashMessages').success('You have signed in successfully');
+        }).catch(
+          ()=> {
+            this.get('flashMessages').warning('Email or password are incorrect');
+        });
     }
   }
 });
